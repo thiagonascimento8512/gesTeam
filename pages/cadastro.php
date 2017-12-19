9ce37b2f903c597b8d433822e5047d721c1917ac
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2017-12-18 23:35:04 -0300

    adicionado balões no formulário

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -3,13 +3,18 @@
 		$nome = $_POST['cmp_nome'];
 		$email = $_POST['cmp_email'];
 		$sexo = $_POST['cmp_sexo'];
-		$tel = $_POST['cmp_tel'];
+		$tel = $_POST['cmp_cel'];
 		$nasc = $_POST['cmp_nasc'];
 		$senha = $_POST['cmp_senha'];		
 		$senha_confirm = $_POST['cmp_senha_confirm'];		
 		
-		
-		
+		echo "Você inseriu as seguintes informações: <br />";
+		echo "Nome: $nome"."<br />";
+		echo "Email: $email"."<br />";
+		echo "Sexo: $sexo"."<br />";
+		echo "Telefone: $tel"."<br />";
+		echo "Data de Nascimento: $nasc"."<br />";
+		echo "Senha: ".$senha."<br />";		
 	}
 ?>
 <!DOCTYPE html>
@@ -18,8 +23,27 @@
 	<meta charset="utf-8" />
 	<title>Novo Cadastro</title>
 	<link href="/css/reset.css" rel="stylesheet"/>
-	<link href="/css/style.css" rel="stylesheet"/>		
-	<script src="../scripts/verificar_tecla.js" type="text/javascript"></script>		
+	<link href="/css/style.css" rel="stylesheet"/>
+	<script src="../js/jquery.js" type="text/javascript"></script>
+	<script src="../scripts/balloon.js" type="text/javascript"></script>
+	<script>
+		window.onload = function(){			
+	  		var password = document.getElementById("cmp_senha"),
+	  		confirm_password = document.getElementById("cmp_senha_confirm");	
+			confirm_password.onchange = verificaSenha;
+			confirm_password.onkeyup = verificaSenha;			
+		}
+		
+		function verificaSenha(){
+			var password = document.getElementById("cmp_senha"),
+		  		confirm_password = document.getElementById("cmp_senha_confirm");	
+			if (password.value == confirm_password.value)
+				$("#salvar").removeAttr("disabled");	
+			else				
+				$("#salvar").attr("disabled","disabled");	
+		}		
+		
+	</script>
 </head>
 <body>
 	<form enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
@@ -38,7 +62,7 @@
 		</select><br />
 		
 		<label for="cmp_tel">Telefone:</label>
-		<input type="tel" name="cmp_cel" id="celular" required="required"/><br />
+		<input type="tel" name="cmp_cel" id="cmp_cel" required="required"/><br />
 		
 		<label for="cmp_nasc">Data de Nascimento:</label>
 		<input type="date" name="cmp_nasc" id="cmp_nasc" required="required"/><br />
@@ -49,7 +73,13 @@
 		<label for="cmp_senha_confirm">Confirme sua senha:</label>
 		<input type="password" name="cmp_senha_confirm" id="cmp_senha_confirm" required="required"/><br />
 		
-		<input type="submit" id="salvar" name="salvar" value="Salvar"/>
+		<input type="submit" id="salvar" name="salvar" value="Salvar" disabled="disabled" />
 	</form>
+	<script>		
+		$('#cmp_nome').balloon(
+			contents:'Thiago',
+			position:'right'
+		);	
+	</script>
 </body>
 </html>
